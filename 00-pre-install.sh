643f005f5959a68ccdfd95c04ca52ff84ced61be
faustouribe <faustouribe@gmail.com>
2025-09-14 12:33:21 -0500

    Update 00-pre-install.sh

--- 00-pre-install.sh
+++ 00-pre-install.sh
@@ -11,5 +11,5 @@ sudo apt install \
 && sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io && sudo usermod -aG docker $USER \
 # Install Extip
 sudo wget https://github.com/arsham/figurine/releases/download/v1.3.0/figurine_linux_amd64_v1.3.0.tar.gz && tar -xvf figurine_linux_amd64_v1.3.0.tar.gz \
-&& cd deploy && sudo mv figurine /usr/bin/ && sudo chmod +x /usr/bin/figurine \
+&& cd deploy && sudo mv figurine /usr/bin/ && cd ../ && sudo rmdir deploy && sudo chmod +x /usr/bin/figurine \
 && sudo cp extip /usr/bin/ && sudo chmod +x /usr/bin/extip && echo "export TERM=xterm" >> ~/.bashrc && echo "export PATH='/home/mrfox/.local/bin:$PATH'" >> ~/.bashrc && echo "extip" >> ~/.bashrc
